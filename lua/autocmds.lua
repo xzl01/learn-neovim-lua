d83ce15161e7b979a5835c4476584a4a77f1cedf
guoyao <guoyao@uniontech.com>
2022-08-03 11:46:25 +0800

    update format

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,14 +5,22 @@ local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
 local autocmd = vim.api.nvim_create_autocmd
 
 -- nvim-tree 自动关闭
-autocmd("BufEnter", {
-  nested = true,
+-- autocmd("BufEnter", {
+--   nested = true,
+--   group = myAutoGroup,
+--   callback = function()
+--     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+--       vim.cmd("quit")
+--     end
+--   end,
+-- })
+
+autocmd({ "BufNewFile", "BufWritePre", "BufRead" }, {
+  pattern = "*",
   group = myAutoGroup,
   callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd("quit")
-    end
-  end,
+    require("basic.files").Settab()
+  end
 })
 
 -- 进入Terminal 自动进入插入模式
@@ -22,11 +30,11 @@ autocmd("TermOpen", {
 })
 
 -- 保存时自动格式化
-autocmd("BufWritePre", {
-  group = myAutoGroup,
-  pattern = { "*.lua", "*.py", "*.sh" },
-  callback = vim.lsp.buf.formatting_sync,
-})
+-- autocmd("BufWritePre", {
+--     group = myAutoGroup,
+--     pattern = { "*.lua", "*.py", "*.sh" },
+--     callback = vim.lsp.buf.formatting_sync,
+-- })
 
 -- 修改lua/plugins.lua 自动更新插件
 autocmd("BufWritePost", {
@@ -55,7 +63,7 @@ autocmd("BufEnter", {
   pattern = "*",
   callback = function()
     vim.opt.formatoptions = vim.opt.formatoptions
-      - "o" -- O and o, don't continue comments
-      + "r" -- But do continue when pressing enter.
+        - "o" -- O and o, don't continue comments
+        + "r" -- But do continue when pressing enter.
   end,
 })
---
+++ lua/basic/files.lua
@@ -0,0 +1,81 @@
+local File = {
+  ["rust"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["cpp"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["typescript"] = {
+    tab = false,
+    shift = 2,
+  },
+  ["cs"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["lua"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["fsharp"] = {
+    tab = false,
+    shift = 2,
+  },
+  ["haskell"] = {
+    tab = false,
+    shift = 2,
+  },
+  ["kotlin"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["xml"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["sh"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["json"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["jsonc"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["python"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["vim"] = {
+    tab = false,
+    shift = 4,
+  },
+  ["zig"] = {
+    tab = false,
+    shift = 4,
+  },
+}
+File.Settab = function()
+  local file = File[vim.o.filetype]
+  if file ~= nil then
+    if file.tab then
+      vim.opt.tabstop = file.shift
+      vim.opt.shiftwidth = file.shift
+      vim.opt.softtabstop = file.shift
+    else
+      --vim.opt.modeline = true
+      vim.opt.shiftwidth = file.shift
+      vim.opt.expandtab = true
+    end
+  else
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.softtabstop = 2
+  end
+end
+return File
